9ed2d17e6c4bb067f9cbb5d7f7a61c98d5787d51
constan1000 <hrairimejdi99@gmail.com>
2021-06-29 10:06:41 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-""" Gather data needed for a given employee ID """
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        respond1 = requests.get('https://jsonplaceholder.typicode.com/todos')
-        respond2 = requests.get('https://jsonplaceholder.typicode.com/users')
-        search1 = respond1.json()
-        search2 = respond2.json()
-        for i in search2:
-            if i['id'] == int(sys.argv[1]):
-                us = i['name']
-        count = 0
-        completed = 0
-        tasks = []
-        for i in search1:
-            for key, value in i.items():
-                if key == 'userId' and value == int(sys.argv[1]):
-                    count += 1
-                    for key, value in i.items():
-                        if key == 'completed' and value is True:
-                            completed += 1
-                            tasks.append(i['title'])
-        print('Employee {} is done with tasks\
-({}/{}):'.format(us, completed, count))
-        for i in tasks:
-            print('\t {}'.format(i))
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [task.get("title") for task in todos if
+                 task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(compl)) for compl in completed]
